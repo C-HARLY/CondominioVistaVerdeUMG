4a3508bed819d12a5b9d4c6ee09094e4fe077fd8
Carlos Mejia <carlosestuardomejia1432005@gmail.com>
2026-04-28 23:20:44 -0600

    Se creo vista login

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module umg.vistaverdeumg {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
    // Esto permite que Scene Builder y JavaFX vean tus controladores
     opens ui to javafx.fxml; 
