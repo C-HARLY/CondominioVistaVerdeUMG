aeb4de8938bc08ba7dd69979f4a02ad7982c36c0
Carlos Mejia <carlosestuardomejia1432005@gmail.com>
2026-05-07 23:54:01 -0600

    se crearon las demas vistas , se implemento el menu principal

--- src/main/java/ui/LoginController.java
+++ src/main/java/ui/LoginController.java
@@ -54,6 +54,8 @@ public class LoginController {
         if (usuario.equals(USER) && password.equals(PASS)) {
             lblMensaje.setStyle("-fx-text-fill: green;");
             lblMensaje.setText("Acceso correcto");
+            
+            abrirMenuPrincipal();
         } else {
             lblMensaje.setStyle("-fx-text-fill: red;");
             lblMensaje.setText("Usuario o contraseña incorrectos");
@@ -62,7 +64,7 @@ public class LoginController {
     
     private void abrirMenuPrincipal() {
     try {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MenuPrincipal.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/MenuPrincipal.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
@@ -72,32 +74,9 @@ public class LoginController {
         stage.show();
 
     } catch (IOException e) {
-        System.err.println("Error al cargar MenuPrincipal: " + e.getMessage());
+       e.printStackTrace();
     }
 }
+
     
-    
-    //METODO PARA MOSTRAR OTRA PANTALLA
-    @FXML // IMPORTANTE: Agrega @FXML para que Scene Builder vea el método
-    public void login() {
-        try {
-            // 1. Cargamos el archivo FXML de la pantalla de pagos
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/Pago.fxml"));
-            Parent root = loader.load();
-
-            // 2. Creamos una nueva "Escena" con ese archivo
-            Scene scene = new Scene(root);
-
-            // 3. Obtenemos la "Ventana" (Stage) actual usando el botón que recibió el clic
-            Stage stage = (Stage) btnLogin.getScene().getWindow();
-
-            // 4. Cambiamos la escena de la ventana actual por la de pagos
-            stage.setScene(scene);
-            stage.show();
-
-        } catch (IOException e) {
-            // Si el nombre del archivo está mal, te avisará aquí
-            System.err.println("Error al cargar la vista de pagos: " + e.getMessage());
-        }
-    }
 }
