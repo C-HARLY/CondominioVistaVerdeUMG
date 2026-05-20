8321e26deed2da004aac172a6cb1d6ea45d93ffc
Karen <karenramos2006.18@gmail.com>
2026-05-19 23:33:31 -0600

    Actualización de interfaz en la pantalla casas morosas

--- src/main/java/ui/CasaMorosaController.java
+++ src/main/java/ui/CasaMorosaController.java
@@ -15,6 +15,9 @@ public class CasaMorosaController implements Initializable {
 
     @FXML
     private ComboBox<String> comboAnios;
+    
+    @FXML
+private ComboBox<String> comboMeses;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -25,5 +28,23 @@ public class CasaMorosaController implements Initializable {
                 "2027",
                 "2028"
         );
+        comboAnios.setValue("2026");
+        comboMeses.getItems().addAll(
+                "Enero",
+                "Febrero",
+                "Marzo",
+                "Abril",
+                "Mayo",
+                "Junio",
+                "Julio",
+                "Agosto",
+                "Septiembre",
+                "Octubre",
+                "Noviembre",
+                "Diciembre"
+        );
+
+        comboMeses.setValue("Enero");
+        
     }
 }
